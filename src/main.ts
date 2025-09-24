9285d6a1bc5507012e00043a5a3b13dae96e3437
khu107 <khu10735@naver.com>
2025-09-24 18:01:08 +0900

    feat: PostgreSQL + Prisma + Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Tarjimon API')
+    .setDescription('The Tarjimon API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
